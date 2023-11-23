b98207e9e5038f3509c3e61cedab0395bb269a35
sharfuddin.zishan <sharfuddin.ahamed91@gmail.com>
2023-11-23 18:34:53 +0600

    All Setup Done

--- src/app.ts
+++ src/app.ts
@@ -1,27 +1,27 @@
-import express, { Application, NextFunction, Request, Response } from 'express'
-import cors from 'cors'
+import express, { Application, NextFunction, Request, Response } from 'express';
+import cors from 'cors';
 
-const app:Application = express()
+const app: Application = express();
 
-app.use(express.json())
-app.use(express.text())
-app.use(cors())
+app.use(express.json());
+app.use(express.text());
+app.use(cors());
 
-const logger=((req:Request,res:Response,next:NextFunction)=>{
-    console.log('Logger');
-    next()
-})
+const logger = (req: Request, res: Response, next: NextFunction) => {
+  console.log('Logger');
+  next();
+};
 
-app.get('/',logger,(req:Request,res:Response)=>{
-    console.log('API Connected');
-    res.send('Everything Worked Fine.')
-})
+app.get('/', logger, (req: Request, res: Response) => {
+  console.log('API Connected');
+  res.send('Everything Worked Fine.');
+});
 
-app.all('**',(req:Request,res:Response)=>{
-    res.status(400).json({
-        message:'No Such Link Exist',
-        success:false
-    })
-})
+app.all('**', (req: Request, res: Response) => {
+  res.status(400).json({
+    message: 'No Such Link Exist',
+    success: false,
+  });
+});
 
-export default app
+export default app;
--- src/server.ts
+++ src/server.ts
@@ -1,18 +1,16 @@
-import app from './app'
-import mongoose from 'mongoose'
-import config from './app/config'
+import app from './app';
+import mongoose from 'mongoose';
+import config from './app/config';
 
-async function main(){
-    try{
-        await mongoose.connect(config.db as string)
-        app.listen(config.port,()=>{
-            console.log(`port ${config.port} Listened Successfully`);
-        })
-    }
-    catch(error){
-        console.log('Database or Port Problem');
-    }
+async function main() {
+  try {
+    await mongoose.connect(config.db as string);
+    app.listen(config.port, () => {
+      console.log(`port ${config.port} Listened Successfully`);
+    });
+  } catch (error) {
+    console.log('Database or Port Problem');
+  }
 }
 
-main()
-
+main();
